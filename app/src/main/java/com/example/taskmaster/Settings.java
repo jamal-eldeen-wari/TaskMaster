f244d619c5c1273c4e21b9a0a6d0606daf357920
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-11-01 13:52:53 +0200

    Adding Lab 27 to github

---
+++ app/src/main/java/com/example/taskmaster/Settings.java
@@ -0,0 +1,51 @@
+package com.example.taskmaster;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toolbar;
+
+public class Settings extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent   = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        SharedPreferences sh = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sh.edit();
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.editTextTextPersonName3);
+                String userName = editText.getText().toString();
+                editor.putString("username",userName);
+                editor.commit();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/taskmaster/TaskDetailPage.java
@@ -0,0 +1,49 @@
+package com.example.taskmaster;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class TaskDetailPage extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail_page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent   = new Intent(TaskDetailPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Intent intentTask1 = getIntent();
+        String task1 = intentTask1.getExtras().getString("task1");
+        TextView textView1 = findViewById(R.id.textView14);
+        textView1.setText(task1);
+
+        Intent intentTask2 = getIntent();
+        String task2 = intentTask2.getExtras().getString("task2");
+        @SuppressLint("CutPasteId") TextView textView2 = findViewById(R.id.textView14);
+        textView2.setText(task2);
+
+        Intent intentTask3 = getIntent();
+        String task3 = intentTask3.getExtras().getString("task3");
+        @SuppressLint("CutPasteId") TextView textView3 = findViewById(R.id.textView14);
+        textView3.setText(task3);
+    }
+}
