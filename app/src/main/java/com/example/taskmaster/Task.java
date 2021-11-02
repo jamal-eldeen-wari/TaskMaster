9c3004d3f26351f7c5d04a8d04cf44c2febcf9d6
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-11-02 17:53:00 +0200

    Adding Lab 28 to github

---
+++ app/src/main/java/com/example/taskmaster/Task.java
@@ -0,0 +1,16 @@
+package com.example.taskmaster;
+
+public class Task {
+    public String title;
+    public String body;
+    public String state;
+
+    public Task(String title, String body, String state) {
+        this.title = title;
+        this.body = body;
+        this.state = state;
+    }
+
+    public Task() {
+    }
+}
