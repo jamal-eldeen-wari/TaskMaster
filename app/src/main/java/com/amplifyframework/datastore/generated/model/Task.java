f3cbdaa5bc278d4d102cb9e325b90c4279320a67
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-11-13 19:05:23 +0200

    adding some stuff

--- app/src/main/java/com/amplifyframework/datastore/generated/model/Task.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/Task.java
@@ -1,5 +1,6 @@
 package com.amplifyframework.datastore.generated.model;
 
+import com.amplifyframework.core.model.annotations.BelongsTo;
 import com.amplifyframework.core.model.temporal.Temporal;
 
 import java.util.List;
@@ -19,15 +20,18 @@ import static com.amplifyframework.core.model.query.predicate.QueryField.field;
 /** This is an auto generated class representing the Task type in your schema. */
 @SuppressWarnings("all")
 @ModelConfig(pluralName = "Tasks")
+@Index(name = "myTasks", fields = {"teamId"})
 public final class Task implements Model {
   public static final QueryField ID = field("Task", "id");
   public static final QueryField TITLE = field("Task", "title");
   public static final QueryField BODY = field("Task", "body");
   public static final QueryField STATE = field("Task", "state");
+  public static final QueryField TEAM = field("Task", "teamId");
   private final @ModelField(targetType="ID", isRequired = true) String id;
   private final @ModelField(targetType="String", isRequired = true) String title;
   private final @ModelField(targetType="String", isRequired = true) String body;
   private final @ModelField(targetType="String", isRequired = true) String state;
+  private final @ModelField(targetType="Team", isRequired = true) @BelongsTo(targetName = "teamId", type = Team.class) Team team;
   private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
   private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
   public String getId() {
@@ -46,6 +50,10 @@ public final class Task implements Model {
       return state;
   }
   
+  public Team getTeam() {
+      return team;
+  }
+  
   public Temporal.DateTime getCreatedAt() {
       return createdAt;
   }
@@ -54,11 +62,12 @@ public final class Task implements Model {
       return updatedAt;
   }
   
-  private Task(String id, String title, String body, String state) {
+  private Task(String id, String title, String body, String state, Team team) {
     this.id = id;
     this.title = title;
     this.body = body;
     this.state = state;
+    this.team = team;
   }
   
   @Override
@@ -73,6 +82,7 @@ public final class Task implements Model {
               ObjectsCompat.equals(getTitle(), task.getTitle()) &&
               ObjectsCompat.equals(getBody(), task.getBody()) &&
               ObjectsCompat.equals(getState(), task.getState()) &&
+              ObjectsCompat.equals(getTeam(), task.getTeam()) &&
               ObjectsCompat.equals(getCreatedAt(), task.getCreatedAt()) &&
               ObjectsCompat.equals(getUpdatedAt(), task.getUpdatedAt());
       }
@@ -85,6 +95,7 @@ public final class Task implements Model {
       .append(getTitle())
       .append(getBody())
       .append(getState())
+      .append(getTeam())
       .append(getCreatedAt())
       .append(getUpdatedAt())
       .toString()
@@ -99,6 +110,7 @@ public final class Task implements Model {
       .append("title=" + String.valueOf(getTitle()) + ", ")
       .append("body=" + String.valueOf(getBody()) + ", ")
       .append("state=" + String.valueOf(getState()) + ", ")
+      .append("team=" + String.valueOf(getTeam()) + ", ")
       .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
       .append("updatedAt=" + String.valueOf(getUpdatedAt()))
       .append("}")
@@ -122,6 +134,7 @@ public final class Task implements Model {
       id,
       null,
       null,
+      null,
       null
     );
   }
@@ -130,7 +143,8 @@ public final class Task implements Model {
     return new CopyOfBuilder(id,
       title,
       body,
-      state);
+      state,
+      team);
   }
   public interface TitleStep {
     BodyStep title(String title);
@@ -143,7 +157,12 @@ public final class Task implements Model {
   
 
   public interface StateStep {
-    BuildStep state(String state);
+    TeamStep state(String state);
+  }
+  
+
+  public interface TeamStep {
+    BuildStep team(Team team);
   }
   
 
@@ -153,11 +172,12 @@ public final class Task implements Model {
   }
   
 
-  public static class Builder implements TitleStep, BodyStep, StateStep, BuildStep {
+  public static class Builder implements TitleStep, BodyStep, StateStep, TeamStep, BuildStep {
     private String id;
     private String title;
     private String body;
     private String state;
+    private Team team;
     @Override
      public Task build() {
         String id = this.id != null ? this.id : UUID.randomUUID().toString();
@@ -166,7 +186,8 @@ public final class Task implements Model {
           id,
           title,
           body,
-          state);
+          state,
+          team);
     }
     
     @Override
@@ -184,9 +205,16 @@ public final class Task implements Model {
     }
     
     @Override
-     public BuildStep state(String state) {
+     public TeamStep state(String state) {
         Objects.requireNonNull(state);
         this.state = state;
+        return this;
+    }
+    
+    @Override
+     public BuildStep team(Team team) {
+        Objects.requireNonNull(team);
+        this.team = team;
         return this;
     }
     
@@ -202,11 +230,12 @@ public final class Task implements Model {
   
 
   public final class CopyOfBuilder extends Builder {
-    private CopyOfBuilder(String id, String title, String body, String state) {
+    private CopyOfBuilder(String id, String title, String body, String state, Team team) {
       super.id(id);
       super.title(title)
         .body(body)
-        .state(state);
+        .state(state)
+        .team(team);
     }
     
     @Override
@@ -222,6 +251,11 @@ public final class Task implements Model {
     @Override
      public CopyOfBuilder state(String state) {
       return (CopyOfBuilder) super.state(state);
+    }
+    
+    @Override
+     public CopyOfBuilder team(Team team) {
+      return (CopyOfBuilder) super.team(team);
     }
   }
   
--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -19,6 +21,7 @@ import android.widget.Toolbar;
 import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.Team;
 
 import java.util.List;
 
@@ -36,6 +39,30 @@ public class AddTask extends AppCompatActivity {
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name").allowMainThreadQueries().build();
         DAO dao = db.dao();
+        String[] teams = new String[]{"Team 1", "Team 2", "Team 3"};
+        RadioGroup radioGroup = findViewById(R.id.radioGroupAddTask);
+        RadioButton radioButton1 = findViewById(R.id.team_1RBtn);
+        RadioButton radioButton2 = findViewById(R.id.team_2RBtn);
+        RadioButton radioButton3 = findViewById(R.id.team_3RBtn);
+        String teamName;
+
+        int getCheckButton = radioGroup.getCheckedRadioButtonId();
+        if (getCheckButton == radioButton1.getId()){
+
+            teamName = radioButton1.getText().toString();
+
+        }else if(getCheckButton == radioButton2.getId()){
+
+            teamName = radioButton2.getText().toString();
+
+        }else if(getCheckButton == radioButton3.getId()){
+
+            teamName = radioButton3.getText().toString();
+
+        }else {
+            teamName = null;
+        }
+
 
 
         TextView textView = findViewById(R.id.textView2);
@@ -54,10 +81,11 @@ public class AddTask extends AppCompatActivity {
 
 //                Task task = new Task(title,description,state);
 //                dao.insertAll(task);
+                Team team = Team.builder().teamName("Gamers").build();
                 Task task = Task.builder()
                         .title(title)
                         .body(description)
-                        .state(state)
+                        .state(state).team(team)
                         .build();
 
                 Amplify.API.mutate(
